a325a946e08f5ea5a87b646d75d68f47e4dd291b
Jasper Tonk <j.c.t.tonk@gmail.com>
2025-07-03 10:29:41 +0200

    Update wind-card.js

--- wind-card.js
+++ wind-card.js
@@ -211,7 +211,7 @@ class WindCard extends LitElement {
             <path class="compass minor" stroke-width="0.8" fill="none" stroke="var(--secondary-text-color, #727272)" stroke-linecap="round" stroke-opacity="1" d="${minorPath}"></path>
           </g>
           <g class="indicators">
-            <g class="marker compass" style="transform: rotate(${this.direction + 180}deg);">
+            <g class="marker compass" transform="rotate(${this.direction + 180},50,50)">
               <path stroke="var(--card-background-color, white)" stroke-linejoin="bevel" d="M 50 97 l 9 -15 l -9 3 l -9 -3 Z" fill="rgb(68,115,158)" stroke-width="0" transform="rotate(180 50 90)"></path>
             </g>
           </g>

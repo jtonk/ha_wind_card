ed0d29b12e4f20539889c8c9bd9328435ec54c58
Jasper Tonk <j.c.t.tonk@gmail.com>
2025-07-12 22:48:23 +0200

    change marker position

--- wind-card.js
+++ wind-card.js
@@ -377,7 +377,7 @@ class WindCard extends LitElement {
               <path class="compass marker"
                 stroke="var(--card-background-color, white)"
                 stroke-linejoin="bevel"
-                d="m 50,${tickPath_radius + 46} l 5,3 l -5,-12 l -5,12 z"
+                d="m 50,${tickPath_radius + 42} l 5,3 l -5,-12 l -5,12 z"
                 fill="var(--primary-text-color, #212121)" stroke-width="0"
                 style="transform: rotate(${this.direction + 180}deg);">
               </path>

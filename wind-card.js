3688ba752eb0f8f3e84d04ad983a8d40fd703d32
Jasper Tonk <j.c.t.tonk@gmail.com>
2025-07-22 12:26:53 +0200

    Update wind-card.js

--- wind-card.js
+++ wind-card.js
@@ -455,7 +455,7 @@ class WindCard extends LitElement {
           <div class="date-wind-bar-segment" style="background:${colorWind};height:${windHeight}px;width:100%;"></div>
           ${gustHeight > 0 ? html`<div class="date-gust-bar-segment" style="background:${colorGust};height:1px;margin-bottom:${gustHeight}px;width:100%;"></div>` : null}
         </div>
-        <ha-icon class="dir-icon" icon="mdi:navigation" style="--mdc-icon-size: 80%; transform: rotate(${direction + 180}deg);"></ha-icon>
+        <ha-icon class="dir-icon" icon="mdi:navigation" style="--mdc-icon-size: 100%; transform: rotate(${direction + 180}deg);"></ha-icon>
       </div>`;
   }
 
